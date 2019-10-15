c9eb2d2c745a939a7cada2d16e4533327e546a4d
Vladimir Zagorski <vlad@st6.io>
2019-10-15 09:54:37 +0300

    nav menu improvements

--- lecture-6/src/App.js
+++ lecture-6/src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
 } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 import Nav from './components/Nav';
 import Routes from './components/Routes';
 import demos from './demos';
+import cx from 'classnames';
 
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div className="App">
-      <div className="App-content">
-        <Switch>
-          <Routes pages={demos} />
-        </Switch>
-      </div>
+const App = () => {
+  const [menuOpened, setMenuOpened] = useState(true);
 
-      <div className="App-nav">
-        <Nav pages={demos} />
+  const handleToggle = () => {
+    setMenuOpened(!menuOpened);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-content">
+          <Switch>
+            <Routes pages={demos} />
+          </Switch>
+        </div>
+
+        <div
+          className={cx('App-nav', {
+            collapsed: !menuOpened,
+          })}
+        >
+          <Nav pages={demos} />
+        </div>
+
+        <button
+          className="App-toggle"
+          onClick={handleToggle}
+        >
+          {menuOpened ? <FaBars /> : <FaTimes />}
+        </button>
       </div>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+};
 
 export default App;
